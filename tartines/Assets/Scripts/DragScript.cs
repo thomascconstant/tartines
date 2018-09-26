1fd97039e6704ff8cc20f912919b4ba49af1f154
William Rao Fernandes <williamraofernandes@gmail.com>
2018-09-26 11:46:58 +0200

    drag script

---
+++ tartines/Assets/Scripts/DragScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScript : MonoBehaviour
+{
+
+
+
+        void OnMouseDrag()
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePos;
+        }
+
+
+}
